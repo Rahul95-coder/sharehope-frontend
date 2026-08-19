34991564bfa1054dba4317a6f70c1cc9db386d3f
Yash <vajayash27092006@gmail.com>
2026-08-19 20:34:47 +0530

    Get user

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await fetch("http://localhost:8080/api/user");
+      const data = await response.json();
+      console.log(data)
+      setUsers(data)
+    };
+
+    fetchUsers();
+  }, []);
 
   return (
     <>
       <h2>Hello</h2>
+
+      {users.map((user) => (
+        <div key={user._id}>
+          <h3>{user.name}</h3>
+          <p>{user.email}</p>
+          <p>{user.role}</p>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
